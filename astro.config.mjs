1da2973f130a2cbad85a6403b5ebcc83aa49d453
Keonwoo Kim <me@kanu.kim>
2025-01-21 17:28:11 +0900

    fix: make everything static and add cloudflare plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: cloudflare(),
 });
